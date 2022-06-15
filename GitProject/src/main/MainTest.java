9163c4ea069c36bc610f23d9632ab23ac85460c4
EnzoT1996 <enzo.trisolini96@gmail.com>
2022-06-15 14:38:26 +0200

    Primo commit Eclipse

---
+++ GitProject/src/main/MainTest.java
@@ -0,0 +1,12 @@
+package main;
+
+import classi.Persona;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		Persona p = new Persona("Fabrizio", "Ponte");
+		System.out.println(p.toString());
+	}
+
+}
